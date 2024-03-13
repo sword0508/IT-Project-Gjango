583b7c58d9d44882fcaa394f98afe9064dc71e30
TransLaTion2004 <2197135820@qq.com>
2024-03-14 00:26:47 +0800

    feat:new

--- ITproject-master/ITproject/views.py
+++ ITproject-master/ITproject/views.py
@@ -17,3 +17,31 @@ def profile(request):
 def tour_details(request):
 
     return render(request, 'tour-details.html')
+def des(request):
+
+    return render(request, 'destination.html')
+def tours(request):
+
+    return render(request, 'tour.html')
+def tes(request):
+
+    return render(request, 'tes.html')
+
+def faq(request):
+
+    return render(request, 'faq.html')
+
+def about(request):
+
+    return render(request, 'about.html')
+
+def blog(request):
+
+    return render(request, 'blog.html')
+
+def bd(request):
+
+    return render(request, 'bd.html')
+def cont(request):
+
+    return render(request, 'cont.html')
